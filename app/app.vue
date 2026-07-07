18ca7843d7281de2c26b5331500c8546b46c09dd
Arthur Ronconi <arthur.ronconi@gmail.com>
2026-07-07 14:37:32 -0300

    init

---
+++ app/app.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <nav class="w-full bg-blue-600 text-white">
+      <ul class="container mx-auto flex items-center gap-2 h-16">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Link 1</a>
+        </li>
+        <li>
+          <a href="#">Link 2</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+  <div class="flex items-center w-full gap-4 container mx-auto py-4">
+    <div class="bg-blue-200 rounded-lg w-full h-96"></div>
+    <div class="rounded-lg">
+      <iframe
+        src="https://main.d2i8s6aclypvu3.amplifyapp.com/"
+        frameborder="0"
+      ></iframe>
+    </div>
+  </div>
+</template>
